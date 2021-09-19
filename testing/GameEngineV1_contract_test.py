7368f536744601de862cb043f49c50de004bbf25
perama-v <83961755+perama-v@users.noreply.github.com>
2021-09-19 17:16:22 +1000

    Add unit tests to evaluate events and logs

--- testing/GameEngineV1_contract_test.py
+++ testing/GameEngineV1_contract_test.py
@@ -49,32 +49,32 @@ async def test_record_items():
     total_locations=40
     location_id = 34
     user_id = 3
-    item_id = 5
+    item_id = 7
+    # Pick a different location in the same suburb (4, 14, 24, 34)
+    random_location = 24
 
     # User has small amount of money, but lots of the item they are selling.
     user_money_pre = 300
     user_item_pre = 55
     # Set action (buy=0, sell=1)
-    buy_or_sell = 1  # Selling
+    buy_or_sell = 0
     # How much is the user giving (either money or item)
-    item_quantity = 20  # 20 of the item.
+    # If selling, it is "give x item". If buying, it is "give x money".
+    give_quantity = 22
 
     # E.g., 10 items in location 1, 20 loc 2.
     sample_item_count_list = [total_locations,
-        10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-        10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-        10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-        10, 20, 30, 42, 50, 60, 70, 80, 90, 100]
+        20, 40, 60, 80, 100, 120, 140, 160, 180, 200,
+        220, 240, 260, 280, 300, 320, 340, 360, 380, 400,
+        420, 440, 460, 480, 500, 520, 540, 560, 580, 600,
+        620, 640, 660, 680, 700, 720, 740, 760, 780, 800]
     # E.g., 100 money in curve for item location 1, 200 loc 2.
     sample_item_money_list = [total_locations,
-        100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-        100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-        100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
-        100, 200, 300, 444, 500, 600, 700, 800, 900, 1000]
+        200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000,
+        2200, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000,
+        4200, 4400, 4600, 4800, 5000, 5200, 5400, 5600, 5800, 6000,
+        6200, 6400, 6600, 6800, 7000, 7200, 7400, 7600, 7800, 8000]
     # Market has lots of money, not a lot of the item it is receiving.
-    # This will use override
-    market_item_pre = sample_item_count_list[location_id]  # val=42.
-    market_money_pre = sample_item_money_list[location_id]  # val=444.
 
     # Create the market.
     # Populate the item pair of interest across all locations.
@@ -92,45 +92,179 @@ async def test_record_items():
     pre_trade_market = await engine_contract.check_market_state(
         location_id, item_id).invoke()
     print('pre_trade_market', pre_trade_market)
+    random_market_pre_turn_item = sample_item_count_list[random_location]
 
     # Execute a game turn.
-    await engine_contract.have_turn(user_id, location_id,
-        buy_or_sell, item_id, item_quantity).invoke()
+    turn = await engine_contract.have_turn(user_id, location_id,
+        buy_or_sell, item_id, give_quantity).invoke()
 
+
+    event_name = [
+        "trade_occurs_bool",
+        "user_pre_trade_item",
+        "user_post_trade_pre_event_item",
+        "user_post_trade_post_event_item",
+        "user_pre_trade_money",
+        "user_post_trade_pre_event_money",
+        "user_post_trade_post_event_money",
+        "market_pre_trade_item",
+        "market_post_trade_pre_event_item",
+        "market_post_trade_post_event_item",
+        "market_pre_trade_money",
+        "market_post_trade_pre_event_money",
+        "market_post_trade_post_event_money",
+        "money_reduction_factor",
+        "item_reduction_factor",
+        "regional_item_reduction_factor",
+        "dealer_dash_bool",
+        "wrangle_dashed_dealer_bool",
+        "mugging_bool",
+        "run_from_mugging_bool",
+        "gang_war_bool",
+        "defend_gang_war_bool",
+        "cop_raid_bool",
+        "bribe_cops_bool",
+        "find_item_bool",
+        "local_shipment_bool",
+        "warehouse_seizure_bool"
+    ]
+    print("Turn events")
+    [
+        print(f"Result: {turn[index]}\t{event_name[index]}")
+        for index in range(len(event_name))
+    ]
+    (
+        trade_occurs_bool,
+        user_pre_trade_item,
+        user_post_trade_pre_event_item,
+        user_post_trade_post_event_item,
+        user_pre_trade_money,
+        user_post_trade_pre_event_money,
+        user_post_trade_post_event_money,
+        market_pre_trade_item,
+        market_post_trade_pre_event_item,
+        market_post_trade_post_event_item,
+        market_pre_trade_money,
+        market_post_trade_pre_event_money,
+        market_post_trade_post_event_money,
+        money_reduction_factor,
+        item_reduction_factor,
+        regional_item_reduction_factor,
+        dealer_dash_bool,
+        wrangle_dashed_dealer_bool,
+        mugging_bool,
+        run_from_mugging_bool,
+        gang_war_bool,
+        defend_gang_war_bool,
+        cop_raid_bool,
+        bribe_cops_bool,
+        find_item_bool,
+        local_shipment_bool,
+        warehouse_seizure_bool
+    ) = turn
+
+    assert market_pre_trade_item == sample_item_count_list[location_id]
+    assert market_pre_trade_money == sample_item_money_list[location_id]
+
+    if dealer_dash_bool == 1 and wrangle_dashed_dealer_bool == 0:
+        assert trade_occurs_bool == 0
+    else:
+        trade_occurs_bool == 1
+
+    # Check market operation
+    if trade_occurs_bool:
+        if buy_or_sell == 1:  # Selling (give item, get money).
+            assert user_pre_trade_item > user_post_trade_pre_event_item
+            assert user_pre_trade_money < user_post_trade_pre_event_money
+            assert market_pre_trade_item < market_post_trade_pre_event_item
+            assert market_pre_trade_money > market_post_trade_pre_event_money
+        if buy_or_sell == 0:  # Buying (give money, get item).
+            assert user_pre_trade_item < user_post_trade_pre_event_item
+            assert user_pre_trade_money > user_post_trade_pre_event_money
+            assert market_pre_trade_item > market_post_trade_pre_event_item
+            assert market_pre_trade_money < market_post_trade_pre_event_money
+    else:
+        if buy_or_sell == 1:
+            assert user_pre_trade_item == user_post_trade_pre_event_item
+            assert user_pre_trade_money == user_post_trade_pre_event_money
+            assert market_pre_trade_item == market_post_trade_pre_event_item
+            assert market_pre_trade_money == market_post_trade_pre_event_money
+        if buy_or_sell == 0:
+            assert user_pre_trade_item == user_post_trade_pre_event_item
+            assert user_pre_trade_money == user_post_trade_pre_event_money
+            assert market_pre_trade_item == market_post_trade_pre_event_item
+            assert market_pre_trade_money == market_post_trade_pre_event_money
+
+
+    # Check no item was minted in the market maker step.
+    assert user_pre_trade_item + market_pre_trade_item == \
+        user_post_trade_pre_event_item + \
+        market_post_trade_pre_event_item
+
+    # Check no money was minted in the market maker step.
+    assert user_pre_trade_money + market_pre_trade_money == \
+        user_post_trade_pre_event_money + \
+        market_post_trade_pre_event_money
+
+    # Determine final event occurrence
+    cop_hit = cop_raid_bool * (1 - bribe_cops_bool)
+    gang_hit = gang_war_bool * (1 - defend_gang_war_bool)
+    mug_hit = mugging_bool * (1 - run_from_mugging_bool)
+
+    # Check event factor logic.
+    if (gang_hit == 1 or cop_hit == 1) and find_item_bool == 0:
+        assert item_reduction_factor < 10
+    if gang_hit == 0 and cop_hit == 0 and find_item_bool == 1:
+        assert item_reduction_factor > 10
+    if mug_hit == 1 or cop_hit == 1:
+        assert money_reduction_factor < 10
+    if local_shipment_bool == 1 and warehouse_seizure_bool == 0:
+        assert regional_item_reduction_factor > 10
+    if local_shipment_bool == 0 and warehouse_seizure_bool == 1:
+        assert regional_item_reduction_factor < 10
+
+    # Check event factors appliied.
+    # Check regional event item effect.
+    assert market_post_trade_post_event_item == \
+        regional_item_reduction_factor * \
+        market_post_trade_pre_event_item // 10
+    # Check regional market money unaffected by events.
+    assert market_post_trade_pre_event_money == (
+        market_post_trade_post_event_money)
+    # Check user money event effect.
+    assert user_post_trade_post_event_money == \
+        money_reduction_factor * \
+        user_post_trade_pre_event_money // 10
+    # Check user item event effect.
+    assert user_post_trade_post_event_item == \
+        item_reduction_factor * user_post_trade_pre_event_item // 10
+
+
+    # Make a separate conract call to assert persistence of state.
     # Inspect post-trade state
     post_trade_user = await engine_contract.check_user_state(
         user_id).invoke()
+    assert post_trade_user[0] == user_post_trade_post_event_money
+    assert post_trade_user[item_id] == user_post_trade_post_event_item
     print('post_trade_user', post_trade_user)
+
     post_trade_market = await engine_contract.check_market_state(
         location_id, item_id).invoke()
+    assert post_trade_market[0] == market_post_trade_post_event_item
+    assert post_trade_market[1] == market_post_trade_post_event_money
     print('post_trade_market', post_trade_market)
 
-    # Check money made. (Assert user money (index 0) post > money pre)
-    assert post_trade_user[0] > user_money_pre
-    # Check gave items. (Assert user item quantity (index=item_id) post > money pre)
-    assert post_trade_user[item_id] < user_item_pre
     # Check location is set
     assert post_trade_user[11] == location_id
 
-
-    # Check that the market gained item
-    assert post_trade_market[0] > market_item_pre
-    # Check that the market lost money
-    assert post_trade_market[1] < market_money_pre
-
-    # Check nothing was minted
-    units_pre = user_money_pre + user_item_pre + market_item_pre + \
-        market_money_pre
-    units_post = post_trade_user[0] + post_trade_user[item_id] \
-        + post_trade_market[0] + post_trade_market[1]
-    assert units_pre == units_post
-
     # Check that another location has been set.
-    random_location = 7
-    (random_item, random_money) = await engine_contract.check_market_state(
+    (random_market_item, random_market_money) = \
+        await engine_contract.check_market_state(
         random_location, item_id).invoke()
-    assert random_money != 0
+    assert random_market_item != 0 and random_market_money != 0
+    # Check that if there was a regional event, it was applied.
+    assert random_market_item == random_market_pre_turn_item * \
+        regional_item_reduction_factor // 10
 
-
-
-
+    # Make false assertion to trigger printing of variables to console.
+    assert 1==0
