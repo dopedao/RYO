e32e5d7540088ab2c2316704dfd096789d2dbd76
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2021-10-06 16:20:00 -0400

    improved exception case handling (inf price); not performing post-game verification until code is refactored

--- testing/GameEngineV1_exerciser_test.py
+++ testing/GameEngineV1_exerciser_test.py
@@ -14,6 +14,7 @@ ITEM_COUNT = 19 # Number of items; item_id in [1,19]
 NUM_SIGNING_ACCOUNTS = MIN_TURN_LOCKOUT*5 # == total user count
 DUMMY_PRIVATE = 123456789987654321
 L1_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 # All accounts currently have the same L1 fallback address.
+N_TURN = 100
 
 # Logging parameters
 COLOR_GREEN = '\33[32m'
@@ -106,16 +107,16 @@ async def test_exerciser(populated_game, populated_registry):
     states of E as well as take action to affect the states of E.
 
     Algorithm:
-    Step 0. Observe pre-game states S, open empty action record AR
+    Step 0. Observe pre-game states S, open empty action record AR (omitted)
     Loop:
         Step 1. Choose P among player pool to make a turn based on turn model (TM)
         Step 2. P assembles action space (A) based on observation model (OM)
         Step 3. P chooses one action (a) from A based on behavior model (BM)
         Step 4. P performs action a against E, add action a to AR
         Step 5. Update TM
-    Step 6. Observe post-game states S'_observed
-    Step 7. Simulate S + AR = S'_expected
-    Step 8. Check S'_observed == S'_expected
+    Step 6. Observe post-game states S'_observed (omitted)
+    Step 7. Simulate S + AR = S'_expected (omitted)
+    Step 8. Check S'_observed == S'_expected (omitted)
 
     For Dope Wars specifically, one implementation could be:
     - Set TM to "random sample while excluding player among disabled-player-list"
@@ -132,23 +133,7 @@ async def test_exerciser(populated_game, populated_registry):
 
     player_ids = [i for i in range(NUM_SIGNING_ACCOUNTS)]
     loc_ids = [i for i in range(LOCATION_COUNT)]
-    item_ids = [i for i in range(1,ITEM_COUNT+1)]
-    N_TURN = 100
-
-    # Step 0. Observe pre-game state and open empty action record
-    a_history = [] # ordered list of turn-by-turn actions
-    market_pre_game = {}
-    player_pre_game = {}
-
-    for loc_id in loc_ids:
-        market_pre_game[loc_id] = {}
-        for item_id in item_ids:
-            curve = await engine.check_market_state(loc_id, item_id).invoke()
-            market_pre_game[loc_id][item_id] = curve
-
-    for player_id in player_ids:
-        p = await engine.check_user_state(player_id).invoke()
-        player_pre_game[player_id] = p
+    item_ids = [i for i in range(1,ITEM_COUNT+1)] # item_id in range [1,ITEM_COUNT]
 
     print(f"> test_exerciser begins with {N_TURN} turns")
     disabled_players = []
@@ -178,9 +163,10 @@ async def test_exerciser(populated_game, populated_registry):
                 #   curve_item * curve_money = (curve_item-1) * (curve_money + X)
                 #   => X = curve_money / (curve_item-1)
                 can_pay_max = int(player_items[0])
-                price_for_one = math.ceil( curve_money/(curve_item-1) ) # if paying less than one item's price, transaction will revert
-                if can_pay_max >= price_for_one: # can't afford even one item!
-                    A.append({ 'type':'buy',  'item_id':item_id, 'max_give_quantity':can_pay_max, 'price_for_one':price_for_one})
+                if curve_item>1: # has more than one item in inventory so that price_for_one != inf:
+                    price_for_one = math.ceil( curve_money/(curve_item-1) ) # if paying less than one item's price, transaction will revert
+                    if can_pay_max >= price_for_one: # otherwise player can't afford even one item!
+                        A.append({ 'type':'buy',  'item_id':item_id, 'max_give_quantity':can_pay_max, 'price_for_one':price_for_one})
 
                 # Calculate can_sell_max == "all my item"
                 can_sell_max = int(player_items[item_id])
@@ -216,35 +202,7 @@ async def test_exerciser(populated_game, populated_registry):
         # TODO: use .format() to format the print
         print(f"> Turn #{turn} completed: player #{player_id}" + color + f" {a['type']} " + ENDC + f"item #{a['item_id']} at location #{loc_id} by giving {give_quantity}.")
 
-        a_history.append({
-            'player_id' : player_id,
-            'loc_id' : loc_id,
-            'item_id' : a['item_id'],
-            'type' : a['type'],
-            'give_quantity' : give_quantity
-        })
-
         # Step 5. Update TM TODO
-
-    # Step 6. Observe post-game state
-    market_post_game = {}
-    player_post_game = {}
-
-    for loc_id in loc_ids:
-        market_post_game[loc_id] = {}
-        for item_id in item_ids:
-            curve = await engine.check_market_state(loc_id, item_id).invoke()
-            market_post_game[loc_id][item_id] = curve
-
-    for player_id in player_ids:
-        p = await engine.check_user_state(player_id).invoke()
-        player_post_game[player_id] = p
-
-    # Step 7. Run simulation to produce expected post-game state
-    # TODO
-
-    # Step 8. Compare observed against expected post-game state
-    # TODO
 
     print("> test_exerciser passes.")
     return
