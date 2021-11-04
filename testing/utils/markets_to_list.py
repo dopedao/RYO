5bdf75bdbcfbca368f1ce2171c3e763a0e30f578
Tarrence van As <tarrence@vanas.family>
2021-11-03 20:36:22 -0500

    refactor: item/money csv as args

--- testing/utils/markets_to_list.py
+++ testing/utils/markets_to_list.py
@@ -1,11 +1,6 @@
 import csv
 import sys
 
-files = [
-    'mappings/initial_markets_money.csv',
-    'mappings/initial_markets_item.csv'
-]
-
 def csv_to_list(file):
     with open(file, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -30,11 +25,11 @@ def test_get_list(name, file):
     print(f'{name} fetched, starts with: {val_list[0:3]} length {len(val_list)}')
     return val_list
 
-def populate_test_markets():
+def populate_test_markets(items, money):
     # Used by pytest
-    money_list = test_get_list('money', files[0])
+    money_list = test_get_list('money', money)
     money_list = [int(i) for i in money_list]
-    item_list = test_get_list('items', files[1])
+    item_list = test_get_list('items', items)
     item_list = [int(i) for i in item_list]
     # Return list of integers.
     return money_list, item_list
@@ -45,5 +40,5 @@ if __name__ == "__main__":
     # python market_to_list.py 0
     # python market_to_list.py 1
 
-    filename = files[int(sys.argv[1])]
+    filename = sys.argv[1]
     get_list(filename)
